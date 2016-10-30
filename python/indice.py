4f7adf01a7a61456c19369246c2d1c6173a2ee6f
slopezv2 <sebaslv13@gmail.com>
2016-10-30 17:30:32 -0500

    Listo el script para indice

--- python/indice.py
+++ python/indice.py
@@ -2,16 +2,17 @@ from mrjob.job import MRJob
 import re
 import os
 import json
+from stop_words import get_stop_words
 WORD_RE = re.compile(r"[\w']+")
-
+language = 'es'
+before = get_stop_words(language, False)
 class MRWordFrequencyCount(MRJob):
     def mapper(self, _, line):
         for word in WORD_RE.findall(line):
             yield word, os.environ["mapreduce_map_input_file"]
 
     def reducer(self, key, values):
-        yield key, list(set(values))
-
-
+        if key not in before:
+            yield key, list(set(values))
 if __name__ == '__main__':
     MRWordFrequencyCount.run()
