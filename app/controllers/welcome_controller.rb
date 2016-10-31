d188fcf9463e54de7316a9759cd0944b53852679
slopezv2 <slopezv2@github.com>
2016-10-31 13:25:35 -0500

    Inicio Expr

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,27 @@ class WelcomeController < ApplicationController
   end
   def busqueda
   end
+  def cargarIndice
+    completo = []
+    ruta_archivo= "/home/sebastian/Documentos/Rails-Projects/Hadoop-Busqueda-Gutenberg/Pruebas-Conexion/Ejemplo"
+    puts "Antes de file"
+    File.open(ruta_archivo).each do |line|
+      texto = line.scan(/"([\w]\S+)"/)
+      texto.each do |parte|
+        parte[0]= parte[0].tr_s('"', '')
+      end
+      completo += texto
+    end
+  end
+
+  private
+  def cargar_arreglo(arreglo)
+    numero = 0
+    arreglo.each do |componente|
+      if numero % 2 != 0
+        
+      end
+      numero++
+    end
+  end
 end
